8792d751cf450dd2b528975c22c2d14eac0d7aff
Arnaud Natal <arnaud.natal@u-bordeaux.fr>
2022-05-18 17:21:03 +0200

    trends

--- Overindebtedness/Debtecon-5-3_Debt_trendsanalysis.do
+++ Overindebtedness/Debtecon-5-3_Debt_trendsanalysis.do
@@ -245,6 +245,10 @@ foreach x in ihs_ISR1 ihs_DAR1 ihs_DSR1 ihs_loanamount1 ihs_annualincome1 ihs_as
 replace `x'=0 if `x'==.
 }
 
+tabstat repay1 repay2 repay3, stat(n mean sd p50)
+tabstat rel_repay1 rel_repay2 rel_repay3, stat(n mean sd p50)
+
+export delimited using "$git\Analysis\Overindebtedness\debttrend.csv", replace
 
 
 ********** Files
@@ -274,72 +278,6 @@ foreach var in annualincome assets_noland loanamount DSR ISR DAR {
 cluster wardslinkage ihs_`var'1 ihs_`var'2 ihs_`var'3, measure(Euclidean)
 cluster dendrogram, cutnumber(100) title("`var'") name(`var'_tree, replace)
 }
-
-***** Relative
-foreach var in formal eco current humank social home repay {
-cluster wardslinkage rel_`var'1 rel_`var'2 rel_`var'3, measure(Euclidean)
-cluster dendrogram, cutnumber(100) title("`var'") name(`var'_tree, replace)
-}
-
-
-********** Partitional
-***** IHS
-*** k=3
-foreach var in annualincome assets_noland loanamount ISR {
-cluster kmeans ihs_`var'1 ihs_`var'2 ihs_`var'3, k(3) measure(Euclidean) start(everyk) name(cl_`var')
-}
-*** k=4
-foreach var in DSR DAR {
-cluster kmeans ihs_`var'1 ihs_`var'2 ihs_`var'3, k(4) measure(Euclidean) start(everyk) name(cl_`var')
-}
-
-***** Rel
-*** k=3
-foreach var in formal eco humank social home {
-cluster kmeans rel_`var'1 rel_`var'2 rel_`var'3, k(3) measure(Euclidean) start(everyk) name(cl_`var')
-}
-
-*** k=4
-foreach var in current repay {
-cluster kmeans rel_`var'1 rel_`var'2 rel_`var'3, k(4) measure(Euclidean) start(everyk) name(cl_`var')
-}
-
-
-********** Reshape
-*** k=3
-reshape long ihs_annualincome ihs_assets_noland ihs_loanamount ihs_DSR ihs_ISR ihs_DAR rel_formal rel_eco rel_current rel_humank rel_social rel_home rel_repay, i(panelvar) j(time)
-
-foreach var in annualincome assets_noland loanamount ISR formal eco humank social home {
-sort cl_`var' panelvar time
-forvalues i=1(1)3{
-twoway (line ihs_`var' time if cl_`var'==`i', c(L) lcolor(black%10)) ///
-, xtitle("Time") ///
-ylabel() ymtick() ytitle("`var'") ///
-title("Cluster `i'") ///
-aspectratio(0.5) graphregion(margin(zero)) plotregion(margin(zero))  ///
-name(gph_`var'_`i', replace)
-}
-}
-
-*** k=4
-foreach var in DSR DAR current repay {
-sort cl_`var' panelvar time
-forvalues i=1(1)4{
-twoway (line ihs_`var' time if cl_`var'==`i', c(L) lcolor(black%10)) ///
-, xtitle("Time") ///
-ylabel() ymtick() ytitle("`var'") ///
-title("Cluster `i'") ///
-aspectratio(0.5) graphregion(margin(zero)) plotregion(margin(zero))  ///
-name(gph_`var'_`i', replace)
-}
-}
-
-
-
-
-
-
-
 
 
 
--- Overindebtedness/dtwclust.R
+++ Overindebtedness/dtwclust.R
@@ -37,8 +37,6 @@ attach(data)
 
 #--- Matrices creation
 
-# Add expenses
-
 X_loan_ihs<-as.matrix(cbind(ihs_loanamount1,ihs_loanamount2,ihs_loanamount3))
 X_income_ihs<-as.matrix(cbind(ihs_annualincome1,ihs_annualincome2,ihs_annualincome3))
 X_assets_ihs<-as.matrix(cbind(ihs_assets_noland1,ihs_assets_noland2,ihs_assets_noland3))
@@ -47,127 +45,210 @@ X_DSR_ihs<-as.matrix(cbind(ihs_DSR1,ihs_DSR2,ihs_DSR3))
 X_DAR_ihs<-as.matrix(cbind(ihs_DAR1,ihs_DAR2,ihs_DAR3))
 X_ISR_ihs<-as.matrix(cbind(ihs_ISR1,ihs_ISR2,ihs_ISR3))
 
-X_agri<-as.matrix(cbind(shareagri1,shareagri2,shareagri3))
-X_nagri<-as.matrix(cbind(sharenagri1,sharenagri2,sharenagri3))
-
-X_info_ihs<-as.matrix(cbind(ihs_informal1, ihs_informal2, ihs_informal3))
-X_form_ihs<-as.matrix(cbind(ihs_formal1, ihs_formal2, ihs_formal3))
-X_econ_ihs<-as.matrix(cbind(ihs_eco1, ihs_eco2, ihs_eco3))
-X_curr_ihs<-as.matrix(cbind(ihs_current1, ihs_current2, ihs_current3))
-X_huma_ihs<-as.matrix(cbind(ihs_humank1, ihs_humank2, ihs_humank3))
-X_soci_ihs<-as.matrix(cbind(ihs_social1, ihs_social2, ihs_social3))
-X_home_ihs<-as.matrix(cbind(ihs_home1, ihs_home2, ihs_home3))
-X_repa_ihs<-as.matrix(cbind(ihs_repay1, ihs_repay2, ihs_repay3))
-
-
-X_info_rel<-as.matrix(cbind(rel_informal1, rel_informal2, rel_informal3))
-X_form_rel<-as.matrix(cbind(rel_formal1, rel_formal2, rel_formal3))
-X_econ_rel<-as.matrix(cbind(rel_eco1, rel_eco2, rel_eco3))
-X_curr_rel<-as.matrix(cbind(rel_current1, rel_current2, rel_current3))
-X_huma_rel<-as.matrix(cbind(rel_humank1, rel_humank2, rel_humank3))
-X_soci_rel<-as.matrix(cbind(rel_social1, rel_social2, rel_social3))
-X_home_rel<-as.matrix(cbind(rel_home1, rel_home2, rel_home3))
-X_repa_rel<-as.matrix(cbind(rel_repay1, rel_repay2, rel_repay3))
-
+# X_info_rel<-as.matrix(cbind(rel_informal1, rel_informal2, rel_informal3))
+# X_form_rel<-as.matrix(cbind(rel_formal1, rel_formal2, rel_formal3))
+# X_econ_rel<-as.matrix(cbind(rel_eco1, rel_eco2, rel_eco3))
+# X_curr_rel<-as.matrix(cbind(rel_current1, rel_current2, rel_current3))
+# X_huma_rel<-as.matrix(cbind(rel_humank1, rel_humank2, rel_humank3))
+# X_soci_rel<-as.matrix(cbind(rel_social1, rel_social2, rel_social3))
+# X_home_rel<-as.matrix(cbind(rel_home1, rel_home2, rel_home3))
+# X_repa_rel<-as.matrix(cbind(rel_repay1, rel_repay2, rel_repay3))
+# 
+# X_loan<-as.matrix(cbind(loanamount1,loanamount2,loanamount3))
+# X_income<-as.matrix(cbind(annualincome1,annualincome2,annualincome3))
+# X_assets<-as.matrix(cbind(assets_noland1,assets_noland2,assets_noland3))
+# X_expenses<-as.matrix(cbind(yearly_expenses1, yearly_expenses2,yearly_expenses3))
+# X_DSR<-as.matrix(cbind(DSR1,ihs_DSR2,DSR3))
+# X_DAR<-as.matrix(cbind(DAR1,ihs_DAR2,DAR3))
+# X_ISR<-as.matrix(cbind(ISR1,ihs_ISR2,ISR3))
 
 
 
 #--- Trends analysis clustering
-interactive_clustering(X_income_ihs)
-interactive_clustering(X_assets_ihs)
-interactive_clustering(X_loan_ihs)
-
-interactive_clustering(X_DSR_ihs)
-interactive_clustering(X_ISR_ihs)
-interactive_clustering(X_DAR_ihs)
-
-interactive_clustering(X_info_rel)
-interactive_clustering(X_form_rel)
-interactive_clustering(X_info_ihs)
-interactive_clustering(X_form_ihs)
-
-interactive_clustering(X_econ_rel)
-interactive_clustering(X_curr_rel)
-interactive_clustering(X_huma_rel)
-interactive_clustering(X_soci_rel)
-interactive_clustering(X_home_rel)
-
-interactive_clustering(X_repa_rel)
-
-interactive_clustering(X_agri)
-interactive_clustering(X_nagri)
-
-
-#--- What to keep?
-
+# interactive_clustering(X_income_ihs)
+# interactive_clustering(X_assets_ihs)
+# interactive_clustering(X_loan_ihs)
+# 
+# interactive_clustering(X_DSR_ihs)
+# interactive_clustering(X_ISR_ihs)
+# interactive_clustering(X_DAR_ihs)
 
 
 #--- Manually trends analysis
-assets<-tsclust(
-  series=X_assets_log,
+income_euc<-tsclust(
+  series=X_income_ihs,
+  type="partitional",
+  k=4,
+  distance="euclidean",
+  centroid="mean",
+  seed=4,
+  trace=TRUE,
+  error.check=TRUE
+)
+
+income_sbd<-tsclust(
+  series=X_income_ihs,
   type="partitional",
   k=4,
   distance="sbd",
-  centroid="median",
+  centroid="pam",
+  seed=3,
+  trace=TRUE,
+  error.check=TRUE
+)
+
+
+
+
+
+assets_euc<-tsclust(
+  series=X_assets_ihs,
+  type="partitional",
+  k=4,
+  distance="euclidean",
+  centroid="mean",
+  seed=8,
+  trace=TRUE,
+  error.check=TRUE
+)
+
+assets_sbd<-tsclust(
+  series=X_assets_ihs,
+  type="partitional",
+  k=4,
+  distance="sbd",
+  centroid="pam",
+  seed=5,
+  trace=TRUE,
+  error.check=TRUE
+)
+
+
+
+
+loan_euc<-tsclust(
+  series=X_loan_ihs,
+  type="partitional",
+  k=4,
+  distance="euclidean",
+  centroid="mean",
+  seed=2,
+  trace=TRUE,
+  error.check=TRUE
+)
+
+loan_sbd<-tsclust(
+  series=X_loan_ihs,
+  type="partitional",
+  k=3,
+  distance="sbd",
+  centroid="pam",
+  seed=2,
+  trace=TRUE,
+  error.check=TRUE
+)
+
+
+
+
+
+DSR_euc<-tsclust(
+  series=X_DSR_ihs,
+  type="partitional",
+  k=5,
+  distance="euclidean",
+  centroid="mean",
   seed=7,
   trace=TRUE,
   error.check=TRUE
 )
 
-income<-tsclust(
-  series=X_income_log,
+DSR_sbd<-tsclust(
+  series=X_DSR_ihs,
+  type="partitional",
+  k=5,
+  distance="sbd",
+  centroid="pam",
+  seed=2,
+  trace=TRUE,
+  error.check=TRUE
+)
+
+
+
+
+ISR_euc<-tsclust(
+  series=X_ISR_ihs,
   type="partitional",
   k=3,
-  distance="sbd",
-  centroid="median",
+  distance="euclidean",
+  centroid="mean",
   seed=1,
   trace=TRUE,
   error.check=TRUE
 )
+
+ISR_sbd<-tsclust(
+  series=X_ISR_ihs,
+  type="partitional",
+  k=5,
+  distance="sbd",
+  centroid="pam",
+  seed=16,
+  trace=TRUE,
+  error.check=TRUE
+)
+
+
+
+
+DAR_euc<-tsclust(
+  series=X_DAR_ihs,
+  type="partitional",
+  k=4,
+  distance="euclidean",
+  centroid="mean",
+  seed=1,
+  trace=TRUE,
+  error.check=TRUE
+)
+
+DAR_sbd<-tsclust(
+  series=X_DAR_ihs,
+  type="partitional",
+  k=4,
+  distance="sbd",
+  centroid="pam",
+  seed=2,
+  trace=TRUE,
+  error.check=TRUE
+)
+
 
 
 
 
 
 #--- Datasets extraction
-cl_annualincome<-income@cluster
-cl_assets_noland<-assets@cluster
-cl_yearly_expenses<-expenses@cluster
+euc_annualincome<-income_euc@cluster
+sbd_annualincome<-income_sbd@cluster
 
-data<-cbind(data,cl_annualincome,cl_assets_noland,cl_yearly_expenses)
+euc_assets_noland<-assets_euc@cluster
+sbd_assets_noland<-assets_sbd@cluster
+
+euc_loanamount<-loan_euc@cluster
+sbd_loanamount<-loan_sbd@cluster
+
+euc_DSR<-DSR_euc@cluster
+sbd_DSR<-DSR_sbd@cluster
+
+euc_ISR<-ISR_euc@cluster
+sbd_ISR<-ISR_sbd@cluster
+
+euc_DAR<-DAR_euc@cluster
+sbd_DAR<-DAR_sbd@cluster
+
+
+data<-cbind(data, euc_annualincome, sbd_annualincome, euc_assets_noland, sbd_assets_noland, euc_loanamount, sbd_loanamount, euc_DSR, sbd_DSR, euc_ISR, sbd_ISR, euc_DAR, sbd_DAR)
 
 write.csv(data,"debttrendRreturn.csv")
-
-
-
-#--- For loan
-detach(data)
-rm(list = ls())
-data<-read.csv("debttrend_v2.csv")
-attach(data)
-X_loan_log<-as.matrix(cbind(log_loanamount1,log_loanamount2,log_loanamount3))
-
-# interactive_clustering(X_loan_log)
-
-loan<-tsclust(
-  series=X_loan_log,
-  type="partitional",
-  k=3,
-  distance="sbd",
-  centroid="median",
-  seed=3,
-  trace=TRUE,
-  error.check=TRUE
-)
-
-cl_loanamount<-loan@cluster
-
-data<-cbind(data,cl_loanamount)
-
-write.csv(data,"debttrendRreturn_v2.csv")
-
-
-#--- Stata graph
-#options("RStata.StataPath" = "C:/Users/Arnaud/Documents/Software/Stata15/StataMP-64")
-#options("RStata.StataVersion" = 15.1)
-#stata("linegraph.do")
